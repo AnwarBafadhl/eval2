1063bb807e9cace5bcd7e601e76f736b4450396c
AnwarBafadhl <442202992@student.ksu.edu.sa>
2023-03-27 12:00:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/eval2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.eval2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    button b;
+    edittext e;
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
