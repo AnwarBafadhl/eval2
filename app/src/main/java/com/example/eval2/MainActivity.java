c1797ee3ed9e99a382d1aa7d3cb8b74d6e13925d
AnwarBafadhl <442202992@student.ksu.edu.sa>
2023-03-27 13:02:28 +0300

    Edit

--- app/src/main/java/com/example/eval2/MainActivity.java
+++ app/src/main/java/com/example/eval2/MainActivity.java
@@ -2,17 +2,36 @@ package com.example.eval2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    button b;
-    edittext e;
+    Button b;
+    EditText e;
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.SendButton);
+        e = findViewById(R.id.PersonName);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = e.getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("PersonName", str);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/eval2/SecondActivity.java
+++ app/src/main/java/com/example/eval2/SecondActivity.java
@@ -2,13 +2,24 @@ package com.example.eval2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    TextView recmsg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        recmsg = findViewById(R.id.msg);
+
+        Intent intent = getIntent();
+
+        String str = intent.getStringExtra("PersonName");
+
+        recmsg.setText("Hello, " + str + ".  Welcome!!!");
     }
 }
